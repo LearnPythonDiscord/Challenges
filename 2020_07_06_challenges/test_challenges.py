5e785bdfd16042ddea34a87d3ce59df8a1f0a9c8
Dan6erbond <moravrav@gmail.com>
2020-07-11 10:12:05 +0200

    Add test descriptions

--- 2020_07_06_challenges/test_challenges.py
+++ 2020_07_06_challenges/test_challenges.py
@@ -8,13 +8,16 @@ class T100BeginnerTests(unittest.TestCase):
     """Tests for the beginner challenges."""
 
     def test_101_is_even(self):
+        """Even numbers entered into the function should return `True`, else `False`."""
         self.assertTrue(challenges.is_even(10))
         self.assertFalse(challenges.is_even(11))
 
     def test_102_is_prime(self):
+        """Prime numbers entered into the function should return `True`, else `False`."""
         self.assertTrue(challenges.is_prime(7))
         self.assertFalse(challenges.is_prime(6))
 
     def test_102_is_mersenne(self):
+        """Mersenne primes entered into the function should return `True`, else `False`."""
         self.assertTrue(challenges.is_mersenne(8))
         self.assertFalse(challenges.is_mersenne(7))
