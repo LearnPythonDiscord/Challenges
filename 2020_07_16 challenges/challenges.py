64f5856c095e43ed028c22474109d2ae5f906111
Dan6erbond <moravrav@gmail.com>
2020-07-17 07:11:36 +0200

    Update style with pep8

--- 2020_07_16 challenges/challenges.py
+++ 2020_07_16 challenges/challenges.py
@@ -2,27 +2,26 @@ class Store:
     def __init__(self, size, sales_month, item_categories, off_license, item_list) -> None:
         '''Vars should be called as inputted, eg size should be self.size'''
         pass
-    
-    def update_monthly(self, value:float) -> None:
+
+    def update_monthly(self, value: float) -> None:
         '''Update monthly sales'''
         pass
-    
-    def add_item(self, item:str) -> None:
+
+    def add_item(self, item: str) -> None:
         '''Update item list'''
         pass
 
     def max_sales(self):
         '''Return max sale value'''
         pass
-    
+
     def min_sales(self):
         '''Return min sale value'''
         pass
-    
+
     def avg_sales(self):
         '''Return average sales'''
         pass
 
     def __repr__(self):
         pass
-
